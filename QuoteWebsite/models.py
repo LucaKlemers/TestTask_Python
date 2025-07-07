fa77f23221437f8fff2578f4408aafac64579e74
Luca <luca.klemmers@gmail.com>
2025-07-07 23:56:24 +0300

    Added upvotes and downvotes, fixed bugs

--- QuoteWebsite/models.py
+++ QuoteWebsite/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.forms import ModelForm
 from django.core.exceptions import ValidationError
 
+
 # The source table.
 # It is the book/movie/song/game/other the Quote is taken from, combined with metadata
 # The quote count should be queried whenever a new Quote is added
@@ -15,35 +16,45 @@ class QuoteSource(models.Model):
         default=0, null=True, blank=True, editable=False
     )  # How many quotes does the source have?
 
-    def clean(self): # Check if valid
-        if self.publishment_date > timezone.now().year and bool(self.published_AC): # Check if publishment year is real
-            raise ValidationError({"publishment_date": "Нельзя процитировать источник из будущего"})
-        
-        def text_clean (string):
-            return ''.join(e for e in string if e.isalnum()).lower() # Clean the quote from special characters, whitespaces. Make lowercase
+    def clean(self):  # Check if valid
+        if self.publishment_date > timezone.now().year and bool(
+            self.published_AC
+        ):  # Check if publishment year is real
+            raise ValidationError(
+                {"publishment_date": "Нельзя процитировать источник из будущего"}
+            )
+
+        def text_clean(string):
+            return "".join(
+                e for e in string if e.isalnum()
+            ).lower()  # Clean the quote from special characters, whitespaces. Make lowercase
+
         cleaned_sources = []
-        for string in QuoteSource.objects.exclude(pk=self.pk): # Creating a list of clean sources
+        for string in QuoteSource.objects.exclude(
+            pk=self.pk
+        ):  # Creating a list of clean sources
             cleaned_sources.append(text_clean(string.name))
         if text_clean(self.name) in cleaned_sources:
-            raise ValidationError( # Check if source already exists
+            raise ValidationError(  # Check if source already exists
                 {"quote_text": "Этот источник уже добавлен"}
             )
+
     def __str__(self):
         if len(self.name) < 35:
             return self.name
         else:
             return str(self.name)[:35] + "..."
-        
 
-class AddSourceForm(ModelForm): # Form for adding sources
+
+class AddSourceForm(ModelForm):  # Form for adding sources
     class Meta:
         model = QuoteSource
-        fields = ["name", "author", "published_AC", "publishment_date"] 
+        fields = ["name", "author", "published_AC", "publishment_date"]
         labels = {
             "name": "Название источника",
             "author": "Имя автора",
             "published_AC": "Опубликован ли источник в нашу эру?",
-            "publishment_date": "Год публикации источника"
+            "publishment_date": "Год публикации источника",
         }
 
 
@@ -55,47 +66,64 @@ class Quote(models.Model):
     # Connecting the source
     source = models.ForeignKey(QuoteSource, on_delete=models.CASCADE)
     # Making ratings 0 at start
-    positive_rating = models.PositiveIntegerField(default=0, editable=False) 
+    positive_rating = models.PositiveIntegerField(default=0, editable=False)
     negative_rating = models.PositiveIntegerField(default=0, editable=False)
     total_rating = models.IntegerField(default=0, editable=False, null=True)
-    
-    def save(self, *args, **kwargs): 
-        other_quotes = Quote.objects.filter(source=self.source).exclude(pk=self.pk).order_by("total_rating", "quote_weight")
+
+    def save(self, *args, **kwargs):
+        other_quotes = (
+            Quote.objects.filter(source=self.source)
+            .exclude(pk=self.pk)
+            .order_by("total_rating", "quote_weight")
+        )
         if len(other_quotes) >= 3:
             other_quotes.first().delete()
-        self.total_rating = self.positive_rating - self.negative_rating  # Update the total rating after rating changes
+        self.total_rating = (
+            self.positive_rating - self.negative_rating
+        )  # Update the total rating after rating changes
         super().save(*args, **kwargs)
-        
-    def clean(self): # Check if valid
-        
-        def text_clean (string):
-            return ''.join(e for e in string if e.isalnum()).lower() # Clean the quote from special characters, whitespaces. Make lowercase
+
+    def clean(self):  # Check if valid
+
+        def text_clean(string):
+            return "".join(
+                e for e in string if e.isalnum()
+            ).lower()  # Clean the quote from special characters, whitespaces. Make lowercase
+
         cleaned_quotes = []
-        for string in Quote.objects.exclude(pk=self.pk): # Creating a list of clean quotes
+        for string in Quote.objects.exclude(
+            pk=self.pk
+        ):  # Creating a list of clean quotes
             cleaned_quotes.append(text_clean(string.quote_text))
-            
+
         if self.quote_weight > 100 or self.quote_weight < 1:
             raise ValidationError(
                 {"quote_weight": "Можно использовать только значения от 1 до 100"}
             )
         elif text_clean(self.quote_text) in cleaned_quotes:
-            raise ValidationError(
-                {"quote_text": "Эта цитата уже добавлена"}
-            )
+            raise ValidationError({"quote_text": "Эта цитата уже добавлена"})
+
     def __str__(self):
         if len(self.quote_text) < 35:
             return self.quote_text
         else:
             return str(self.quote_text)[:35] + "..."
-class AddQuoteForm(ModelForm): # Form for adding quotes
+
+    def check_vote(self, request):
+        cookie = f'{self.pk}_vote'
+        return request.COOKIES.get(cookie)
+
+class AddQuoteForm(ModelForm):  # Form for adding quotes
     class Meta:
         model = Quote
-        fields = ["quote_text", "quote_weight", "source"] 
+        fields = ["quote_text", "quote_weight", "source"]
         labels = {
             "quote_text": "Текст цитаты",
             "quote_weight": "Вес цитаты, влияющий на шанс её выпадения (1-100)",
             "source": "Источник цитаты (книга, фильм и т.п.)",
         }
+
+
 class PageViews(models.Model):
     name = models.CharField(default="views")
     views = models.PositiveIntegerField(default=0)
--- QuoteWebsite/views.py
+++ QuoteWebsite/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Quote, QuoteSource, AddQuoteForm, AddSourceForm, PageViews
 from random import choices
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Quote, QuoteSource, AddQuoteForm, AddSourceForm, PageViews
 
 
 def index(request):  # Main page
@@ -19,12 +20,70 @@ def index(request):  # Main page
             )
         selected_quote = choices(all_quotes, weights=influenced_weights, k=1)
         return selected_quote[0]
-    # Claculate views
-    page_views, created = PageViews.objects.get_or_create(name="views")
-    page_views.views += 1
-    page_views.save()
-    context = {"selected_quote": random_quote(0.25), "views": page_views.views}
-    return render(request, "QuoteWebsite/index.html", context)
+
+    page_views = PageViews.objects.get_or_create(name="views")[0]
+    if request.POST.get("vote") == "upvote" or request.POST.get("vote") == "downvote":
+        selected_quote_id = request.COOKIES.get("quote_cookie")
+        selected_quote = (
+            Quote.objects.get(pk=selected_quote_id) if selected_quote_id else None
+        )
+        vote_type = request.POST.get("vote")
+        cookie = f"{selected_quote.pk}_vote"
+        current_vote = request.COOKIES.get(cookie)
+        context = {"selected_quote": selected_quote, "views": page_views.views}
+        request.session["was_button"] = True
+        response = HttpResponseRedirect(reverse("index"))
+        # Vote toggling
+        if vote_type == current_vote:
+            if vote_type == "upvote":
+                selected_quote.positive_rating -= 1
+            elif vote_type == "downvote":
+                selected_quote.negative_rating -= 1
+            response.delete_cookie(cookie)
+        else:
+            if current_vote == "upvote":
+                selected_quote.positive_rating -= 1
+            elif current_vote == "downvote":
+                selected_quote.negative_rating -= 1
+
+            if vote_type == "upvote":
+                selected_quote.positive_rating += 1
+            elif vote_type == "downvote":
+                selected_quote.negative_rating += 1
+
+            response.set_cookie(  # Adding a cookie
+                cookie,
+                vote_type,
+                max_age=30 * 24 * 60 * 60,
+                httponly=True,
+                samesite="Lax",
+            )
+        selected_quote.save()
+        return response
+    elif request.session.get("was_button"):
+        request.session["was_button"] = False
+        selected_quote_id = request.COOKIES.get("quote_cookie")
+        selected_quote = (
+            Quote.objects.get(pk=selected_quote_id) if selected_quote_id else None
+        )
+        context = {"selected_quote": selected_quote, "views": page_views.views}
+        response = render(request, "QuoteWebsite/index.html", context)
+        return response
+    else:
+        page_views.views += 1
+        page_views.save()
+        selected_quote = random_quote(0.25)
+        context = {"selected_quote": selected_quote, "views": page_views.views}
+        response = render(request, "QuoteWebsite/index.html", context)
+        response.delete_cookie("quote_cookie")
+        response.set_cookie(
+            "quote_cookie",
+            selected_quote.pk,
+            max_age=30 * 24 * 60 * 60,
+            httponly=True,
+            samesite="Lax",
+        )
+        return response
 
 
 def add_quote(request):  # Page to add new quotes
@@ -44,22 +103,51 @@ def add_quote(request):  # Page to add new quotes
     return render(request, "QuoteWebsite/add_quote.html", {"form": form})
 
 
-def vote(request, quote_id):  # Like/dislike buttons
-    return HttpResponse(
-        "Upvote or downvote buttons for the quote indexed %s will be here" % quote_id
-    )
+def vote(request, selected_quote):  # Like/dislike buttons
+    vote_type = request.POST.get("vote")
+    cookie = f"{selected_quote.pk}+_vote"
+    current_vote = request.COOKIES.get(cookie)
+    response = HttpResponse(status=204)
+    # Vote toggling
+    if vote_type == current_vote:
+        if vote_type == "upvote":
+            selected_quote.positive_rating -= 1
+        elif vote_type == "downvote":
+            selected_quote.negative_rating -= 1
+        response.delete_cookie(cookie)
+    else:
+        if current_vote == "upvote":
+            selected_quote.positive_rating -= 1
+        elif current_vote == "downvote":
+            selected_quote.negative_rating -= 1
+
+        if vote_type == "upvote":
+            selected_quote.positive_rating += 1
+        elif vote_type == "downvote":
+            selected_quote.negative_rating += 1
+
+        response.set_cookie(  # Adding a cookie
+            cookie, vote_type, max_age=30 * 24 * 60 * 60, httponly=True, samesite="Lax"
+        )
+    selected_quote.save()
+    return response
 
 
 def statistics(request):  # Statistics page (top quotes, etc.)
-    top_quotes = Quote.objects.order_by("total_rating")[:10]
-    context = {"top_quotes": top_quotes,}
+    top_quotes = Quote.objects.order_by("-total_rating")[:10]
+    context = {
+        "top_quotes": top_quotes,
+    }
     return render(request, "QuoteWebsite/statistics.html", context)
 
 
 def source_info(request, source_id):  # Source pages
     quote_source = get_object_or_404(QuoteSource, pk=source_id)
     source_quotes = Quote.objects.filter(source=quote_source)
-    context = {"quote_source": quote_source, "source_quotes": source_quotes,}
+    context = {
+        "quote_source": quote_source,
+        "source_quotes": source_quotes,
+    }
     return render(request, "QuoteWebsite/source_info.html", context)
 
 
